9f1f0005f42485f6f1ae6ea1c07307a3500e051f
Milton Calnek <milton@calnek.com>
2014-08-12 22:06:04 -0600

    Add contact info.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 #
 # Home
 #
   describe "Home Page" do
+    let(:myurl) {"/static_pages/home"}
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit "#{myurl}"
 
       expect(page).to have_content('Sample App')
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it "should have the title base title" do
+      visit "#{myurl}"
+
+      expect(page).to have_title("#{base_title}")
     end
 
   end # Home
@@ -20,12 +27,18 @@ describe "Static pages" do
 # Help 
 #
   describe "Help Page" do
+    let(:myurl) {"/static_pages/help"}
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit "#{myurl}"
 
       expect(page).to have_content('Help')
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
+    it "should have a title base_title | Help" do
+      visit "#{myurl}"
+
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end # Help
@@ -34,15 +47,43 @@ describe "Static pages" do
 # About Us
 #
   describe "About Us Page" do
+    let(:myurl) {"/static_pages/about"}
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit "#{myurl}"
 
       expect(page).to have_content('About Us')
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+
+    it "should have the title base_title | About Us" do
+      visit "#{myurl}"
+
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
   end # about us
+
+#
+# Contact Us
+#
+  describe "Contact Us Page" do
+    let(:myurl) {"/static_pages/contact"}
+
+    it "should have the content 'Contact Us'" do
+      visit "#{myurl}"
+
+      expect(page).to have_content('Contact Us')
+      expect(page).to have_content('306 581 5046')
+      expect(page).to have_content('milton@calnek.com')
+    end
+
+    it "should have the title base_title | Contact Us" do
+      visit "#{myurl}"
+
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
+
+  end # Contact us
 
 end # Static pages
 
