88f4d600951bd004c8abdf9d0400a4829699e054
Milton Calnek <milton@calnek.com>
2014-08-25 22:44:29 -0600

    Flush out test cases.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get user_pages_index_path
-      response.status.should be(200)
-    end
-  end
-end
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	subject { page }
+
+  describe "Sign up page" do
+		before { visit signup_path }
+    it { should have_content('Sign Up') }
+    it { should have_title("#{base_title} | Sign Up") }
+  end # Sign up
+end # user pages
